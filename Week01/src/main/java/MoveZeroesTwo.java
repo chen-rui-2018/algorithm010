c6e9259a28632aee566c6c2a4cb0c4ee280396f7
chenr <chenr@op-mobile.com.cn>
2020-06-11 23:47:11 +0800

    两数之和

--- Week01/src/main/java/MoveZeroesTwo.java
+++ Week01/src/main/java/MoveZeroesTwo.java
@@ -1,10 +1,52 @@
-package PACKAGE_NAME;
-
 /**
  * @author： chenr
  * @date： Created on 2020/6/11 23:23
  * @version： v1.0
  * @modified By:
+ * 移动0 第二遍
  */
 public class MoveZeroesTwo {
+    public static void main(String[] args) {
+        int [] arr = {0,1,0,3,12};
+        Solution  solution = new MoveZeroesTwo().new Solution();
+
+        solution.moveZeroes(arr);
+        System.out.println();
+        solution.moveZeroes2(arr);
+    }
+    class Solution {
+        // 双指针, 记录0的个数, 非0元素向前填充,循环完成后,将数组后的0个数位 赋值为0;
+        public void moveZeroes(int[] nums) {
+            int j = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    nums[j]=nums[i];
+                    j++;
+                }
+            }
+            for (int i = j; i < nums.length; i++) {
+                nums[i] = 0;
+            }
+            for (int i = 0; i < nums.length; i++) {
+                System.out.print(nums[i]+",");
+            }
+        }
+        // 双指针, 将0 和非零值 以 0 为边界 划分到左右两边, 非零到左 0 到右
+        public void moveZeroes2(int[] nums) {
+            int j = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    j++;
+                }
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                System.out.print(nums[i]+",");
+            }
+        }
+
+    }
 }
--- Week01/src/main/java/P1TwoSum.java
+++ Week01/src/main/java/P1TwoSum.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author： chenr
@@ -7,4 +8,68 @@ package PACKAGE_NAME;
  * @modified By:
  */
 public class P1TwoSum {
+    public static void main(String[] args) {
+        Solution solution = new P1TwoSum().new Solution();
+        int [] nums = {2, 7, 11, 15};
+        int target = 17;
+        int [] aim = solution.twoSum(nums,target);
+        for (int i=0; i<aim.length;i++){
+            System.out.print(aim[i]+",");
+        }
+        System.out.println();
+        int [] aim1 = solution.twoSum1(nums,target);
+        for (int i=0; i<aim.length;i++){
+            System.out.print(aim[i]+",");
+        }
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        //暴力破解 时间复杂度 O(n^2)
+        public int[] twoSum(int[] nums, int target) {
+            // 目标数组,存返回数组
+            int [] aim = new int[2];
+            for (int i = 0; i<nums.length-1; i++){
+                for (int j=i+1;j<nums.length; j++) {
+                    if(nums[i] + nums [j] == target){
+                        aim[0] = i;
+                        aim[1] = j;
+                        break;
+                    }
+                }
+            }
+            return aim;
+        }
+        // 两遍哈希表
+        public int[] twoSum1(int[] nums, int target) {
+            // hashMap 保存 一遍 nums 的值;
+            Map<Integer,Integer> map = new HashMap<>(16);
+            for (int i = 0; i < nums.length; i++) {
+                map.put(nums[i],i);
+            }
+
+            for (int i = 0; i < nums.length; i++){
+                int complement = target - nums[i];
+                if (map.containsKey(complement) && map.get(complement) != i) {
+                    return new int[]{i,map.get(complement)};
+                }
+            }
+            throw new IllegalArgumentException("No two sum solution");
+        }
+        // 一遍hash 表
+        public int[] twoSum2(int[] nums, int target) {
+            // hashMap 保存 一遍 nums 的值;
+            Map<Integer,Integer> map = new HashMap<>(16);
+            for (int i = 0; i < nums.length; i++) {
+                int complement = target - nums[i];
+                if(map.containsKey(complement)){
+                    return new int[]{map.get(complement),i};
+                }
+                map.put(nums[i],i);
+            }
+            throw new IllegalArgumentException("No two sum solution");
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
 }
